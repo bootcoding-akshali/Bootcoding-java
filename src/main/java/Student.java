1a41b88bebaa7a662feb70e0ae7eba0fd63d22cf
bootcoding#123 <ModakAkshali211@gmail.com>
2022-11-01 14:23:31 +0530

    Student.java

---
+++ src/main/java/Student.java
@@ -0,0 +1,31 @@
+public class Student {
+
+
+    private String id;
+    private String city;
+    private double percentage;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+}
