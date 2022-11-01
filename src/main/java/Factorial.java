28695362d13c3245a29d7a37518ad8176dba6f40
bootcoding#123 <ModakAkshali211@gmail.com>
2022-11-01 11:27:58 +0530

    Factorial.java

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,12 @@
+public class Factorial {
+
+    public static void main(String[] args) {
+        int a, fact=1;
+        int no = 3;
+        for (int i=1; i<no;i++){
+            fact=fact*i;
+
+        }
+        System.out.println(fact);
+    }
+}
