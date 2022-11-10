5bc7e76d5981e6da21f108b86b06599b961dc1d5
bootcoding#123 <ModakAkshali211@gmail.com>
2022-11-10 20:25:00 +0530

    TotalAmountInterest

---
+++ src/main/java/bootcoding/basicmatths/Bank.java
@@ -0,0 +1,20 @@
+package bootcoding.basicmatths;
+
+public class Bank {
+    public static void main(String[] args) {
+        calculateInterest(100, 5,10);
+    }
+    public  static void calculateInterest (int amount, int years, double interestRate ){
+
+        double interestRateMultiplier = 1 + interestRate / 100;
+        double totalAmount = amount;
+
+        for (int i=0; i<5;i++){
+
+            totalAmount  = totalAmount  * interestRateMultiplier;
+        }
+        System.out.println(totalAmount);
+
+        
+    }
+}
