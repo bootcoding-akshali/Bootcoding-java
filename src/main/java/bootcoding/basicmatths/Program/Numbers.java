14fe2e0314dab4086ca403c4ab3f1823e838e257
bootcoding#123 <ModakAkshali211@gmail.com>
2022-11-18 11:46:42 +0530

    1 to 10 print numbers

---
+++ src/main/java/bootcoding/basicmatths/Program/Numbers.java
@@ -0,0 +1,14 @@
+package bootcoding.basicmatths.Program;
+
+public class Numbers {
+
+    public static void printNumbers(){
+        for (int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        printNumbers();
+    }
+}
