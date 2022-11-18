9d748701aa73a9c90ec751ae8fa0b032348a3e17
bootcoding#123 <ModakAkshali211@gmail.com>
2022-11-18 11:40:08 +0530

    Log

---
+++ src/main/java/Excercise/Loop.java
@@ -0,0 +1,19 @@
+package Excercise;
+
+public class Loop {
+    public static void loop(int n) {
+        int i = n;
+        while (i > 1) {
+            System.out.println(i);
+            if (i % 2 == 0) {
+                i = i / 2;
+            } else {
+                i = i + 1;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        loop(5);
+    }
+}
---
+++ src/main/java/Excercise/WhileLoop.java
@@ -0,0 +1,14 @@
+package Excercise;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int n=4;
+        while (n>0){
+            n= n-1;
+            System.out.println(n);
+        }
+
+    }
+
+
+}
