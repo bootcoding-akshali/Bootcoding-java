b9e1a64ba9d8568a46e30550a64ae21790be2051
bootcoding#123 <ModakAkshali211@gmail.com>
2022-11-19 13:05:17 +0530

    Armstrong.java FactorialNo.java FindFactorial.java SumOFSeries.java

---
+++ src/main/java/bootcoding/basicmatths/Armstrong.java
@@ -0,0 +1,24 @@
+package bootcoding.basicmatths;
+
+public class Armstrong {
+    public static void main(String[] args) {
+      printArmstrongNumbers(153);
+    }
+    public  static  void printArmstrongNumbers(int n){
+//     int num1 = 10;
+//     int num2 = 20;
+
+        for(int i=1; i<=500;i++) {
+            int d = i;
+            int result = 0;
+            while (d > 0) {
+                int x = d % 10;
+                result = result + (x * x * x);
+                d = d / 10; // 1
+            }
+            if (i == result) {
+                System.out.println(result);
+            }
+        }
+    }
+}
