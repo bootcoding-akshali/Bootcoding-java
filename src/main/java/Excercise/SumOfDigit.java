07442bb235eb34e862ddc74d4e436e4e04e7e81b
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-02 12:29:06 +0530

    SumOfDigit

--- src/main/java/Excercise/SumOfDigit.java
+++ src/main/java/Excercise/SumOfDigit.java
@@ -2,16 +2,51 @@ package Excercise;
 
 public class SumOfDigit {
     public static void main(String[] args) {
-        int n = 567;
-        System.out.print("Sum of digits : "+ addDigit(n));
+//        int n = 567;
+//        System.out.print("Sum of digits : "+ addDigit(n));
+       // int i = addDigits(38);
+       // System.out.println(i);
+        int num = 88;
+        System.out.println(num == 0 ? 0 : 1 + (num - 1) % 9);
+//        int res = addDigitsIntoSingleDigit(998989);
+//        System.out.println(res);
+
+
     }
-    private static int addDigit(int no)
-    {
-        if (no == 0)
-        {
-            return 0;
+    public static int addDigitsIntoSingleDigit (int num){
+        int sum = 0;
+        if(num > 0 && num < 9){
+            return num;
         }
-        return (no % 10 + addDigit(no / 10));
+        while(num != 0){
+            int d = num % 10;
+            sum = sum + d;
+            num = num / 10;
+            if(num == 0 && sum > 9){
+                System.out.println("sum = " + sum);
+                num = sum;
+                sum = 0;
+            }
+        }
+
+        return sum;
     }
+
+
+//    private static int addDigit(int no)
+//    {
+//        if (no == 0)
+//        {
+//            return 0;
+//        }
+//        return (no % 10 + addDigit(no / 10));
+//    }
+        public static int addDigits(int num) {
+          while (num==0){
+              return 0;
+          }
+          return (num % 10 + addDigits(num/10));
+
+}
 
 }
