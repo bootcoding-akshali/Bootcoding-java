3037871f4455414e503c223ae8f8a10815d3fd56
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-05 14:29:51 +0530

    TotalNumberString TwoNumber

---
+++ src/main/java/TestQuestion/TotalNumberString.java
@@ -0,0 +1,18 @@
+package TestQuestion;
+//Create a function that takes two strings as arguments and return either true or false
+//        depending on whether the total number of characters in the first string is equal to the total number of characters in the second string.
+public class TotalNumberString {
+    public static void main(String[] args) {
+       String str1 = "Hello";
+       String str2 = "Hello";
+        boolean totalNo = findTotalNo(str1, str2);
+        System.out.println(totalNo);
+
+    }
+    public static boolean  findTotalNo(String str1 , String str2){
+         if (str1.equals(str2)) {
+             return true;
+         }
+         return false;
+    }
+}
---
+++ src/main/java/TestQuestion/TwoNumber.java
@@ -0,0 +1,10 @@
+package TestQuestion;
+
+public class TwoNumber {
+    public static void main(String[] args) {
+
+    }
+    public static void getTwoNumber(int a , int b){
+
+    }
+}
