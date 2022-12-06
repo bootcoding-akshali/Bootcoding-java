db175e0798d0a5f4d866abe2f32a38be7860f849
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-06 11:16:42 +0530

    TestQuestions

---
+++ src/main/java/TestQuestion/SumOFArray.java
@@ -0,0 +1,18 @@
+package TestQuestion;
+//Calculate the sum of only positive numbers within an array using method with return sum
+public class SumOFArray {
+    public static void main(String[] args) {
+      int [] array= {1,5,-3,2,11};
+      calculateSumOfArray(array,0);
+    }
+    public static int calculateSumOfArray(int[] array , int sum){
+        for (int i =0; i<array.length; i++) {
+            if (array[i] > 0) {
+                sum += array[i];
+                System.out.println(sum);
+            }
+        }
+        return sum;
+
+    }
+}
