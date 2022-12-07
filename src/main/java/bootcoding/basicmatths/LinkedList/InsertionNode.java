c8fb1f41786c5c45500638a1e3dc8c966bade17e
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-07 11:31:05 +0530

    InsertionNode

--- src/main/java/bootcoding/basicmatths/LinkedList/InsertionNode.java
+++ src/main/java/bootcoding/basicmatths/LinkedList/InsertionNode.java
@@ -2,9 +2,14 @@ package bootcoding.basicmatths.LinkedList;
 
 public class InsertionNode {
     public static void main(String[] args) {
-        Node node = FirstInsertData();
-        printFirstInsertNode(node);
-       FirstInsertData();
+        Node head = createDummyList();
+        printList(head);
+        lastInsertNode(head);
+        System.out.println("After Last Insert");
+        printList(head);
+      head = firstInsertNode(head);
+        System.out.println("After first insert");
+       printList(head);
 
     }
     static class Node{
@@ -17,29 +22,43 @@ public class InsertionNode {
         }
     }
 
-    private static void printFirstInsertNode(Node head){
+    private static  Node firstInsertNode(Node head){
+        Node newNode = new Node(100);
+        newNode.next = head;
+        head = newNode;
+       return head;
+    }
+    private static void lastInsertNode(Node head){
         Node temp = head;
+        Node fifithnode = new Node(50);
         System.out.println(head);
-        while (temp != null){
+        while (temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = fifithnode;
+    }
+
+    public static void printList(Node head){
+        Node temp = head;
+        while (temp != null) {
             System.out.println(temp.data);
             temp = temp.next;
         }
     }
 
-    private static Node FirstInsertData(){
-        Node newNode = new Node(100);
+
+    private static Node createDummyList(){
         Node firstNode = new Node(10);
         Node secondNode = new Node(20);
         Node thirdNode = new Node(30);
         Node forthNode = new Node(40);
+
 
         Node head = firstNode;
         head.next = secondNode;
         secondNode.next = thirdNode;
         thirdNode.next = forthNode;
 
-        newNode.next =head;
-        head =newNode;
         return head;
     }
 
