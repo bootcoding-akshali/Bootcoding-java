c0a0a84d2e363345054190da10d7377a00832244
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-15 14:53:59 +0530

    StringMethods

---
+++ src/main/java/bootcoding/basicmatths/String/Replace.java
@@ -0,0 +1,8 @@
+package bootcoding.basicmatths.String;
+
+public class Replace {
+    public static void main(String[] args) {
+        String str1 = "bat ball";
+        System.out.println(str1.replace("b", "c"));
+    }
+}
