5f57e77b96a857adaef1296bc8e0ddc015047585
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-15 12:03:41 +0530

    printAnyMultiTable

---
+++ src/main/java/bootcoding/basicmatths/program/MultiRecursion.java
@@ -0,0 +1,16 @@
+package bootcoding.basicmatths.program;
+
+public class MultiRecursion {
+    public static void main(String[] args) {
+
+          printMultipleNo(3,1);
+    }
+    public static void printMultipleNo(int number, int i){
+
+        System.out.println(i*number);
+        if (i == 10){
+            return;
+        }
+        printMultipleNo(number,i+1);
+    }
+}
