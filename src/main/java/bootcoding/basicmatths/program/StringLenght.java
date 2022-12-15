feb70bff0c92461d12cba8036aa322684129c1e2
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-15 11:59:50 +0530

    StringLenght

---
+++ src/main/java/bootcoding/basicmatths/program/StringLenght.java
@@ -0,0 +1,13 @@
+package bootcoding.basicmatths.program;
+
+public class StringLenght {
+    public static void main(String[] args) {
+        String s = "Akshali Modak";
+        System.out.println(s);
+
+        int length = s.length();
+        System.out.println("String length is : "+length);
+    }
+
+
+}
