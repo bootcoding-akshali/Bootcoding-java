4ae0b90558b210782b5e4e18c08ebd8f752aac40
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-23 12:40:28 +0530

    Assignments

---
+++ src/main/java/bootcoding/basicmatths/assignments/PrintNumber.java
@@ -0,0 +1,15 @@
+package bootcoding.basicmatths.assignments;
+
+public class PrintNumber {
+    public static void main(String[] args) {
+          calculateNumbers();
+    }
+    public static void calculateNumbers(){
+          int  a = -5 + 8 * 6;
+           int b = (55+9) % 9;
+          int  c =20 + -3*5 / 8;
+           int d = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(a+" "+b+" "+c+" "+d);
+
+    }
+}
