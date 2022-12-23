84db7adf04e500ecd96d66278d489b4bb2e29947
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-23 13:12:33 +0530

    Assignments

--- src/main/java/bootcoding/basicmatths/assignments/Month.java
+++ src/main/java/bootcoding/basicmatths/assignments/Month.java
@@ -2,33 +2,14 @@ package bootcoding.basicmatths.assignments;
 
 public class Month {
     public static void main(String[] args) {
-       printMonth(8);
+        int num = 8;
+       printMonth(num);
     }
-    public static void printMonth(int monthNo){
-        if (monthNo == 1)
-            System.out.println("January");
-        else if (monthNo == 2)
-            System.out.println("February");
-        else if (monthNo== 3)
-            System.out.println("March");
-        else if (monthNo == 4)
-            System.out.println("April");
-        else if (monthNo == 5)
-            System.out.println("May");
-        else if (monthNo == 6)
-            System.out.println("June");
-        else if (monthNo== 7)
-            System.out.println("July");
-        else if (monthNo == 8)
-            System.out.println("August");
-        else if (monthNo == 9)
-            System.out.println("September");
-        else if (monthNo == 10)
-            System.out.println("October");
-        else if (monthNo == 11)
-            System.out.println("November");
-        else if (monthNo == 12)
-            System.out.println("December");
+    public static void printMonth(int num){
+        String[] months = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+
+        System.out.println(months[num-1]);
+
 }
 
 }
