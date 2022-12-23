19a927ada06a7bf5cf979cab139faa78651f4bf6
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-23 14:53:10 +0530

    Assignments

---
+++ src/main/java/bootcoding/basicmatths/assignments/EmployeeId.java
@@ -0,0 +1,14 @@
+package bootcoding.basicmatths.assignments;
+
+public class EmployeeId {
+    public static void main(String[] args) {
+    printEmployeeId(0342,8,15000);
+    }
+    public static void printEmployeeId(int id,int hr, int amt){
+        int salary;
+        salary = hr*amt;
+        System.out.println(salary);
+
+     }
+    }
+
