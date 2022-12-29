7c2dc2eef86465e7ce05ab788c2d36421c058c15
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-29 20:52:17 +0530

    Pattern

---
+++ src/main/java/bootcoding/basic/matths/pattern/InvertedHalfPyramidNo.java
@@ -0,0 +1,17 @@
+package bootcoding.basic.matths.pattern;
+
+public class InvertedHalfPyramidNo {
+    public static void main(String[] args) {
+        invertedHalfPyramidWithNo(5);
+    }
+    public static void invertedHalfPyramidWithNo(int n){
+        //outer loop
+        for (int i=1;i<=n;i++){
+            //inner loop
+            for (int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println( );
+        }
+    }
+}
