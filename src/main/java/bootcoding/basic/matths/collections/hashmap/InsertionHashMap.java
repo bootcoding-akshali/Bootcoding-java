48289b35ba5e95b59958b5b5d95bb367fcddad2a
bootcoding#123 <ModakAkshali211@gmail.com>
2023-01-02 20:38:31 +0530

    Hashmap

---
+++ src/main/java/bootcoding/basic/matths/collections/hashmap/InsertionHashMap.java
@@ -0,0 +1,19 @@
+package bootcoding.basic.matths.collections.hashmap;
+
+import java.util.HashMap;
+
+public class InsertionHashMap {
+    public static void main(String[] args) {
+        //country(key) , population(value)
+        HashMap<String , Integer> map = new HashMap<>();
+
+        //insertion
+        map.put("India",120);
+        map.put("US",30);
+        map.put("China",150);
+
+        System.out.println(map);
+    }
+
+}
+
