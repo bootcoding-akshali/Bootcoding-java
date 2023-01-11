8df02331eb0f1629b0e867eb6d408ab4a1c3487c
bootcoding#123 <ModakAkshali211@gmail.com>
2023-01-11 22:26:04 +0530

    StringCompare.Ex

---
+++ src/main/java/bootcoding/basic/matths/String/Compair.java
@@ -0,0 +1,19 @@
+package bootcoding.basic.matths.String;
+
+public class Compair {
+    public static void main(String[] args) {
+        String name1 = "tony";
+        String name2 = "tony22";
+
+        //s1 > s2 : +ve value
+        //s1 == s2 : 0
+        //s1 < s2 : -ve value
+
+        if (name1.compareTo(name2)==0){
+            System.out.println("String are equal");
+        }
+        else {
+            System.out.println("String are not equal");
+        }
+    }
+}
