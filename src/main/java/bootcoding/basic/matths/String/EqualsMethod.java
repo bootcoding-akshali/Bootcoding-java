430d99f87ffed31901e6e08870f6d50c43254187
bootcoding#123 <ModakAkshali211@gmail.com>
2023-01-14 23:42:24 +0530

    EqualsMethod StringExample

---
+++ src/main/java/bootcoding/basic/matths/String/EqualsMethod.java
@@ -0,0 +1,10 @@
+package bootcoding.basic.matths.String;
+
+public class EqualsMethod {
+    public static void main(String[] args) {
+        String s1 = "Aman";
+        String s2 = "Aman";
+        String s3 = new String("Aman");
+        System.out.println(s1.equals(s3));
+    }
+}
