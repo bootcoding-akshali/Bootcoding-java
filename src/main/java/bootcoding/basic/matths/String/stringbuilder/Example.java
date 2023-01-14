f5ac5bfd9c43612b0192e9de07697e9187ef3808
bootcoding#123 <ModakAkshali211@gmail.com>
2023-01-14 22:52:25 +0530

    StringBuilder Example

---
+++ src/main/java/bootcoding/basic/matths/String/stringbuilder/Example.java
@@ -0,0 +1,18 @@
+package bootcoding.basic.matths.String.stringbuilder;
+
+public class Example {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Tony");
+        System.out.println(sb);
+
+        //char at index 0
+        System.out.println(sb.charAt(0));
+        //set  char at index 0
+        sb.setCharAt(0,'p');
+        System.out.println(sb);
+
+        // insert
+        sb.insert(2,'n');
+        System.out.println(sb);
+    }
+}
