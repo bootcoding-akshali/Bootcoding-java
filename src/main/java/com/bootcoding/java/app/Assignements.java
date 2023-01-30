2b144380dc907db2b0c82f14686639c520bd3465
bootcoding#123 <ModakAkshali211@gmail.com>
2023-01-30 23:29:08 +0530

    OrderDAO,Order-service,pom.xml file

---
+++ src/main/java/com/bootcoding/java/app/Assignements.java
@@ -0,0 +1,16 @@
+package com.bootcoding.java.app;
+
+public class Assignements {
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println("Even number is: ");
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + "");
+            }
+
+
+        }
+    }
+}
