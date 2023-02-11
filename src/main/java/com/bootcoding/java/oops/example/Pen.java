dc908d9485cf287d6b75e0f47ff7b0aebb3f002e
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-11 14:27:27 +0530

    OOPS EX PEN

---
+++ src/main/java/com/bootcoding/java/oops/example/Pen.java
@@ -0,0 +1,9 @@
+package com.bootcoding.java.oops.example;
+
+public class Pen {
+    String color;
+    public void printColor() {
+        System.out.println("The color of this Pen is " + this.color);
+    }
+
+}
