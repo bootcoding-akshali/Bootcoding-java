f57c8c684c5e5b29b9f58b1c6160f3d927b3562b
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-17 21:05:31 +0530

    BirdWatcher Example

---
+++ src/main/java/bird/watcher/CountForFirstDaysBirds.java
@@ -0,0 +1,22 @@
+package bird.watcher;
+
+public class CountForFirstDaysBirds {
+    int[] birdsPerDay = {0, 2, 5, 3, 7, 8, 4};
+
+    public int getCountForFirstDays(int givenDaysToCount) {
+
+            int totalBirdsForgivenDays = 0;
+            if(givenDaysToCount >= birdsPerDay.length) {
+                return givenDaysToCount;
+            }
+            for(int i = 0; i < givenDaysToCount; i++) {
+                totalBirdsForgivenDays = totalBirdsForgivenDays + birdsPerDay[i];
+            }
+           return totalBirdsForgivenDays;
+        }
+
+    public static void main(String[] args) {
+        CountForFirstDaysBirds countForFirstDaysBirds = new CountForFirstDaysBirds();
+        System.out.println(countForFirstDaysBirds.getCountForFirstDays(4));
+    }
+}
