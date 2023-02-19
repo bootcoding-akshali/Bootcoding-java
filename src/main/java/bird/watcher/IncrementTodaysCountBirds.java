0475554e56cb44e5e537e359426cae9563a8300b
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-19 23:25:48 +0530

    Refactor

--- src/main/java/bird/watcher/IncrementTodaysCountBirds.java
+++ src/main/java/bird/watcher/IncrementTodaysCountBirds.java
@@ -3,10 +3,9 @@ package bird.watcher;
 public class IncrementTodaysCountBirds {
     int[] birdsPerDay = { 0, 2, 5, 3, 7, 8, 4};
     public int incrementTodaysCount() {
-      int[] birdsPerDay = { 0, 2, 5, 3, 7, 8, 4};
+//      int[] birdsPerDay = { 0, 2, 5, 3, 7, 8, 4};
         int todaysBirdCount = birdsPerDay[birdsPerDay.length-1];
         return todaysBirdCount + 1;
-
     }
 
     public static void main(String[] args) {
