e9c14d1490803a326d40fd918ab3bb7982dcde34
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-19 23:18:39 +0530

    Refactor

--- src/main/java/bird/watcher/BirdWatcher.java
+++ src/main/java/bird/watcher/BirdWatcher.java
@@ -1,26 +1,59 @@
 package bird.watcher;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BirdWatcher {
+    static int[] birdsPerDay = { 0, 2, 5, 3, 7, 8, 4};
+    public BirdWatcher(int[] birdsPerDay){
+        this.birdsPerDay = birdsPerDay;
+    }
 
-    public void getLastWeek() {
+         public int[] getLastWeek() {
+          return birdsPerDay;
+    }
+    public int getToday() {
+        return birdsPerDay.length-1;
+    }
+      public int incrementTodaysCount() {
+          int[] birdsPerDay = { 0, 2, 5, 3, 7, 8, 4};
+        int todaysBirdCount = birdsPerDay[birdsPerDay.length-1];
+        int incrementedtodaysBirdCount = todaysBirdCount + 1;
+         return incrementedtodaysBirdCount;
 
-        int[] birdsPerDay = { 0, 2, 5, 3, 7, 8, 4};
-
-        List<Integer> birdCameLastWeek = new ArrayList<>();
-
-        for(int birdCount: birdsPerDay) {
-            birdCameLastWeek.add(birdCount);
+    }
+     public boolean hasDayWithoutBirds() {
+        for (int birdsCountForToday : birdsPerDay) {
+            if (birdsCountForToday == 0) {
+                return true;
+            }
         }
-        System.out.println("birds count for last week : "+birdCameLastWeek);
+        return false;
     }
+        public int getCountForFirstDays(int  givenDaysToCount ) {
+            int totalBirdsForgivenDays = 0;
 
-    public static void main(String[] args) {
-        BirdWatcher birdWatcher = new BirdWatcher();
-        birdWatcher.getLastWeek();
+            if(givenDaysToCount >= birdsPerDay.length) {
+                return givenDaysToCount;
+            }
+            for(int i = 0; i < givenDaysToCount; i++) {
+                totalBirdsForgivenDays = totalBirdsForgivenDays + birdsPerDay[i];
+            }
+           return totalBirdsForgivenDays;
+        }
 
-    }
+        public int getBusyDays() {
 
+            int busyDay = 0;
+            for(int i = 0; i < birdsPerDay.length; i++) {
+                if(birdsPerDay[i] >= 5) {
+                    busyDay++;
+                }
+            }
+            return busyDay;
+        }
+        public static void main(String[] args) {
+        BirdWatcher birdWatcher = new BirdWatcher(birdsPerDay);
+           int[] lastWeek = birdWatcher.getLastWeek();
+           System.out.println(lastWeek[6]);
+           birdWatcher.getToday();
+
+       }
 }
