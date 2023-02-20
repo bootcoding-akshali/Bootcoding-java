e625f29f4185a87b0c876bf33741ed07f0ae8ab4
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-20 23:10:01 +0530

    Find even no in array

---
+++ src/main/java/excercise/FindEvenNoArray.java
@@ -0,0 +1,28 @@
+package excercise;
+
+public class FindEvenNoArray {
+        public static void main(String[] args) {
+            int[] num = {1,2, 7, 3, 15};
+            System.out.println("Array Elements");
+            for (int i = 0; i < num.length; i++) {
+                System.out.println(num[i]);
+            }
+            findPrimeElements(num);
+
+        }
+
+        private static void findPrimeElements(int[] num) {
+            int[] a = num;
+            for (int i =0;i<num.length;i++){
+                if (num[i]==2 || num[i] == 3){
+                    System.out.println(num[i]+" is Prime Number");
+                }else if (num[i]%2==0){
+
+                }else if (num[i]%3==0){
+
+                }else {
+                    System.out.println(num[i]+" is Prime Number");
+                }
+            }
+        }
+}
