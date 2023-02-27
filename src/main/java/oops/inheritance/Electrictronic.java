81bfe85e61f5f9ae49d9dea18bf0d126cc8c4cdc
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-27 17:42:08 +0530

    Inhiritance product example

---
+++ src/main/java/oops/inheritance/Electrictronic.java
@@ -0,0 +1,12 @@
+package oops.inheritance;
+
+public class Electrictronic extends Product{
+    private String brand;
+
+    public static void main(String[] args) {
+     Product product = new Product();
+     product.homeGoods();
+    }
+}
+
+
