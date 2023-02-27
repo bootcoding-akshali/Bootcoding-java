5095f25512b6f663337b69c68902c3967acba407
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-27 17:41:52 +0530

    Inhiritance product example

---
+++ src/main/java/oops/inheritance/Product.java
@@ -0,0 +1,11 @@
+package oops.inheritance;
+
+public class Product {
+    private String name;
+    private double price;
+    private String description;
+
+    void homeGoods(){
+        System.out.println("wood");
+    }
+}
