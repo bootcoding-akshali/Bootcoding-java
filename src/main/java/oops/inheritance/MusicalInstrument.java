2aeb275ea97252e6f88a5f468190bb6d23ab8fef
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-28 23:52:50 +0530

    Inheritance Musical instruments examples

---
+++ src/main/java/oops/inheritance/MusicalInstrument.java
@@ -0,0 +1,15 @@
+package oops.inheritance;
+
+public class MusicalInstrument {
+    protected String name;
+    protected String category;
+
+    public MusicalInstrument(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public void play() {
+        // Placeholder method for playing the instrument
+    }
+}
