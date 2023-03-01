2fcd113b8b49ad48e0a5f16adafc3fa748f8e016
bootcoding#123 <ModakAkshali211@gmail.com>
2023-03-01 20:46:35 +0530

    Inheritance Musical instruments examples Different types of Play the songs

--- src/main/java/oops/inheritance/Application.java
+++ src/main/java/oops/inheritance/Application.java
@@ -4,11 +4,13 @@ import com.bootcoding.java.String.StringExample;
 
 public class Application {
     public static void main(String[] args) {
-        StringInstrument instrument = new StringInstrument("ins");
+        StringInstrument instrument = new StringInstrument("The harp");
+        instrument.play();
         instrument.pluckString();
-        WindInstrument windInstrument = new WindInstrument("wind");
+        WindInstrument windInstrument = new WindInstrument("birds");
         windInstrument.blow();
-        PercussionInstrument percussionInstrument = new PercussionInstrument("Hit");
+        PercussionInstrument percussionInstrument = new PercussionInstrument("thud");
         percussionInstrument.hit();
+
     }
 }
