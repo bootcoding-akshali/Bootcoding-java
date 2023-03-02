33054e81959ace2fa23bce7cc147fdf459acf99c
bootcoding#123 <ModakAkshali211@gmail.com>
2023-03-02 23:14:15 +0530

    HashMap Emplyee practice quetion

---
+++ src/main/java/com/bootcoding/java/collections/hashmap/Employee.java
@@ -0,0 +1,14 @@
+package com.bootcoding.java.collections.hashmap;
+
+import java.util.HashMap;
+
+public class Employee {
+    public static void main(String[] args) {
+        HashMap<String,Integer> emp = new HashMap<>();
+        emp.put("pratik",1);
+        emp.put("Ram",2);
+        emp.put("Sham",3);
+        System.out.println(emp);
+
+    }
+}
