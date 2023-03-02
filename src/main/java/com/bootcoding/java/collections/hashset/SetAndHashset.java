00edb517f918f22f2eb8f5a921fe459e364595cb
bootcoding#123 <ModakAkshali211@gmail.com>
2023-03-02 23:49:50 +0530

    HashSet example practice

---
+++ src/main/java/com/bootcoding/java/collections/hashset/SetAndHashset.java
@@ -0,0 +1,17 @@
+package com.bootcoding.java.collections.hashset;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetAndHashset {
+    public static void main(String[] args) {
+        Set<String> names = new HashSet<>();
+        names.add("pritam");
+        names.add("Kajal");
+        names.add("pavan");
+        names.add("kajal");
+
+        names.remove(0);
+        System.out.println(names.size());
+    }
+}
