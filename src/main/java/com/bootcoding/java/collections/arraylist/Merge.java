60df29fa6a4affb83c43a8770a009a992c1d51ba
bootcoding#123 <ModakAkshali211@gmail.com>
2023-03-05 23:31:58 +0530

    Arraylist MergeList example

---
+++ src/main/java/com/bootcoding/java/collections/arraylist/Merge.java
@@ -0,0 +1,18 @@
+package com.bootcoding.java.collections.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Merge {
+    public static void main(String[] args) {
+        List<String> list1 = new ArrayList<>();
+        list1.add("1");
+        list1.add("5");
+        List<String> list2 = new ArrayList<>();
+        list2.add("2");
+
+        List<String> mergedList = new ArrayList<>(list1);
+        mergedList.addAll(list2);
+        System.out.println(mergedList); // [1, 2]
+    }
+}
