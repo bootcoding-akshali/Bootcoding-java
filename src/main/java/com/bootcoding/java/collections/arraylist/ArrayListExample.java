cbccd13d6c610fe1a32fe7429c6d79d9804cda80
bootcoding#123 <ModakAkshali211@gmail.com>
2023-03-07 21:46:15 +0530

    Arraylist  example

--- src/main/java/com/bootcoding/java/collections/arraylist/ArrayListExample.java
+++ src/main/java/com/bootcoding/java/collections/arraylist/ArrayListExample.java
@@ -14,35 +14,35 @@ public class ArrayListExample {
         System.out.println(list);
 
 //        //to get an element
-//        int element = list.get(0); // 0 is the index
-//        System.out.println(element);
-//
-//        //add element in between
-//        list.add(1,2); // 1 is the index and 2 is the element to be added
-//        System.out.println(list);
+         int element = list.get(0); // 0 is the index
+         System.out.println(element);
 
-//        //set element
-//        list.set(0,0);
-//        System.out.println(list);
+         //add element in between
+         list.add(1,2); // 1 is the index and 2 is the element to be added
+         System.out.println(list);
+
+         //set element
+         list.set(0,0);
+        System.out.println(list);
 
         //delete elements
         list.remove(2); // 0 is the index
         System.out.println(list);
 
-//        //size of list
-//        int size = list.size();
-//        System.out.println(size);
-//
-//        //Loops on lists
-//        for(int i=0; i<list.size(); i++) {
-//            System.out.print(list.get(i) + " ");
-//        }
-//        System.out.println();
-//
-//        //Sorting the list
-//        list.add(0);
-//        Collections.sort(list);
-//        System.out.println(list);
+        //size of list
+        int size = list.size();
+        System.out.println(size);
+
+        //Loops on lists
+        for(int i=0; i<list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+
+        //Sorting the list
+        list.add(0);
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
 
