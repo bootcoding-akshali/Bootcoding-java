9b53bc7978be9a9bdee37351890831310cbf2f77
bootcoding#123 <ModakAkshali211@gmail.com>
2023-03-09 23:11:23 +0530

    Compare two integer no

---
+++ src/main/java/excercise/CompareNo.java
@@ -0,0 +1,14 @@
+package excercise;
+
+public class CompareNo {
+    public static void main(String[] args) {
+        int num1 = 100;
+        int num2 = 100;
+         if (num1 == num2){
+             System.out.println("equal");
+         }
+         else {
+             System.out.println("not equal");
+         }
+    }
+}
